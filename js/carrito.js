43225e1ef312050b7a35b156c91ffd36c28944dc
facundo yagodnig <facundoyagodnig@gmail.com>
2022-01-11 11:23:59 -0300

    agregamos funcionalidades del carrito

--- js/carrito.js
+++
@@ -1,3 +0,0 @@
-function iniCarrito() {
-    console.warn('iniCarrito()')
-}
--- js/main.js
+++ js/main.js
@@ -1,97 +1,64 @@
-// let contadorTv = 0;
-// let contadorLa = 0;
-// let btnMinus = document.querySelectorAll("#btn-minus");
-// let btnPlus = document.querySelectorAll("#btn-plus");
-// let productTitle = document.querySelectorAll(".card__heading");
+class Main {
 
-// btnMinus[0].addEventListener("click", function () {
-//   if (contadorTv === 0) {
-//     alert("Ya no tienes mas productos que quitar!");
-//   } else {
-//     contadorTv--;
-//     console.log(
-//       `${productTitle[0].textContent}. Unidades compradas: ${contadorTv}`
-//     );
-//   }
-// });
+ async ajax (url, metodo='get') {
+  return await fetch(url, {method : metodo}).then(r => r.text())
+  };
 
-// btnMinus[1].addEventListener("click", function () {
-//   if (contadorLa === 0) {
-//     alert("Ya no tienes mas productos que quitar!");
-//   } else {
-//     contadorLa--;
-//     console.log(
-//       `${productTitle[1].textContent}. Unidades compradas: ${contadorLa}`
-//     );
-//   }
-// });
-
-// btnPlus[0].addEventListener("click", function () {
-//   contadorTv++;
-//   console.log(
-//     `${productTitle[0].textContent} Unidades compradas: ${contadorTv}"`
-//   );
-// });
-// btnPlus[1].addEventListener("click", function () {
-//   contadorLa++;
-//   console.log(
-//     `${productTitle[1].textContent} Unidades compradas: ${contadorLa}"`
-//   );
-// });
-
-const ajax = (url, metodo) => {
-  let xhr = new XMLHttpRequest();
-  xhr.open(metodo || 'get', url)
-  xhr.send();
-
-  return xhr;
-};
-
-const getFileName = (id) => {
+ getFileName(id)  {
   return 'vistas/' + id + '.html'};
 
-const cargarPlantilla = (id) => {
- 
-  let fileName = getFileName(id);
-  let xhr = ajax(fileName);
-  xhr.addEventListener("load", () => {
-    if (xhr.status == 200) {
-      let plantillas = xhr.response;
-      /* Carga del codigo vista html de la plantilla */
-      let main = document.querySelector("main");
-      main.innerHTML = plantillas;
-      /* Carga de los scripts */
-      initJS(id)
-    }
-  });
-};
+  marcarLink(id) {
+    let links = document.querySelectorAll('header nav a')
+    links.forEach( link => {
+        if(link.id == id) link.classList.add('active')
+        else link.classList.remove('active')
+    })
+}
 
-function initJS(id){
+initJS(id){
 
   if(id == 'alta'){
-initAlta()}
+    initAlta()}
 
- else if(id == 'inicio'){
-initInicio()}
+  else if(id == 'inicio'){
+   initInicio()}
 
- else if(id == 'nosotros'){
-initNosotros()}
+  else if(id == 'nosotros'){
+    initNosotros()}
 
- else if(id == 'contacto'){
-initContacto()}
+  else if(id == 'contacto'){
+   initContacto()}
 
- else if(id == 'carrito'){
-initCarrito()}
+  else if(id == 'carrito'){
+    initCarrito()}
 
 }
 
-function getPlantillas(){
+ async cargarPlantilla(id){
+ 
+    let fileName = this.getFileName(id);
+    let plantilla = await this.ajax(fileName)
+    // Carga del código de vista (HTML) de la plantilla
+    let main = document.querySelector('main')
+    main.innerHTML = plantilla
+
+     // Carga del código script (JS) de la plantilla
+     this.initJS(id)
+
+  }
+    
+  
+
+
+ async getPlantillas(){
   /* Carga inicial de la plantilla, segun la url visitada */
   let id = location.hash.slice(1) || 'inicio'; //si no esta definido, sale inicio
-  cargarPlantilla(id);
+  this.marcarLink(id)
+  await this.cargarPlantilla(id);
 
   /* Carga de cada uno de los contenidos segun la navegacion local */
   let links = document.querySelectorAll("header nav a");
+
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
@@ -102,10 +69,19 @@ function getPlantillas(){
     });
   });
 
-  window.addEventListener("hashchange", () => {
-    let id = location.hash.slice(1) || 'inicio'; //si no esta definido, sale inicio
-    cargarPlantilla(id);
-  });
-};
+  window.addEventListener("hashchange", async () => {
+      let id = location.hash.slice(1) || 'inicio'; //si no esta definido, sale inicio
+      this.marcarLink(id)
+      await this.cargarPlantilla(id);
+    });
+  }
 
-getPlantillas();
+  async start() {
+    await this.getPlantillas()
+  }
+
+}
+
+
+const main = new Main()
+main.start()
