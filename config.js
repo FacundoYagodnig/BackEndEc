2f505e8d9442d64c1b6cafc0acacbe5a8f50ba76
facundo yagodnig <facundoyagodnig@gmail.com>
2022-02-04 11:00:22 -0300

    fixed bug update and adding payment info

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export default {
     PORT : 8080,
     TIPO_DE_PERSISTENCIA_PRODUCTOS : "MONGODB",  //"MEM" "FILE" "MONGODB"
     TIPO_DE_PERSISTENCIA_CARRITO : "MONGODB",  //"MEM" "FILE" "MONGODB"
-    STR_CNX : 'mongodb+srv://fyagodnig:8JMzb3rudic6rvg@cluster0.nwfrl.mongodb.net/ecommerce-first-database?retryWrites=true&w=majority'
+    STR_CNX : 'STR CNX'
 
 }
